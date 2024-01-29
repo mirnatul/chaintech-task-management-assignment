eeb94901436621ecc5c41c8e3859fc4bf29c0435
mirnatul <www.mirnatul@gmail.com>
2024-01-29 14:39:04 +0600

    navbar added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Layout/Main';
+import Home from './pages/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className='max-w-[1420px] mx-auto px-2 md:px-8'>
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>,
 )
